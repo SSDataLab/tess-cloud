125381f643d3786a1f532b1dd216da76160e1a37
Geert Barentsen <hello@geert.io>
2021-02-26 21:05:28 -0800

    :recycle: Refactor manifest.py using boto3 instead of s3fs

--- src/tess_cloud/manifest.py
+++ src/tess_cloud/manifest.py
@@ -1,5 +1,10 @@
 from functools import lru_cache
+import io
 import os
+
+import boto3
+from botocore import UNSIGNED
+from botocore.config import Config
 
 import diskcache
 import pandas as pd
@@ -11,21 +16,27 @@ CACHEDIR = os.path.join(os.path.expanduser("~"), ".tess-cloud-cache")
 cache = diskcache.Cache(directory=CACHEDIR)
 
 
+def get_boto3_client():
+    return boto3.client("s3", config=Config(signature_version=UNSIGNED))
+
+
 @cache.memoize(expire=86400)  # on-disk cache
-def load_manifest():
-    """Returns a DataFrame listing all the TESS files available on AWS S3."""
-    return pd.read_fwf('s3://stpubdata/tess/public/manifest.txt.gz',
-                       compression='gzip',
-                       names=['modified_date', 'modified_time', 'size', 'path'])
+def load_manifest_lookup():
+    """Returns a hashtable mapping filename => S3 URI"""
+    s3c = get_boto3_client()
+    obj = s3c.get_object(Bucket="stpubdata", Key="tess/public/manifest.txt.gz")
+    df = pd.read_fwf(io.BytesIO(obj['Body'].read()),
+                     compression='gzip',
+                     names=['modified_date', 'modified_time', 'size', 'path'])
+    # Filter out the FITS files
+    fits_files = df[df.path.str.contains("fits")]
+    # Make a lookup hashtable which maps filename => path
+    lookup = dict(zip(fits_files.path.str.split("/").str[-1], fits_files.path))
+    return lookup
 
 
 @lru_cache  # in-memory cache
 def get_cloud_uri(filename):
-    """Returns the AWS S3 URI of a TESS file."""
-    manifest = load_manifest()
-    result = manifest[manifest.path.str.endswith(filename)]
-    if len(result) == 1:
-        return "s3://" + TESS_S3_BUCKET + "/" + result.path.iloc[0]
-    elif len(result) > 1:
-        raise ValueError("AWS Manifest contains duplicate images")
-    return None
+    """Returns the S3 URI of a TESS data product given its filename."""
+    lookup = load_manifest_lookup()
+    return "s3://stpubdata/" + lookup[filename]
